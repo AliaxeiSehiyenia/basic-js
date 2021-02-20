9ac5fd1e3e62b446e0925521c50567cc5cd2c202
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-20 18:36:31 +0300

    feat: change function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,62 +1,30 @@
 module.exports = function transform(arr) {
-  let myArr = arr;
-  let length = myArr.length - 1;
-  let indexDiscardNext = 0;
-  let indexDiscardPrev = 0;
-  let indexDoubleNext = 0;
-  let indexDoublePrev = 0;
-  if (Array.isArray(myArr)) {
-    if (myArr.length === []) {
-      return [];
-    } else {
-    for (let i = 0; i < myArr.length; i++) {
-      switch(myArr[i]) {
-        case '--discard-next':
-          indexDiscardNext = myArr.indexOf(myArr[i]);
-          if (indexDiscardNext === length) {
-            myArr.splice(indexDiscardNext, 1)
-          } else {
-            myArr.splice(indexDiscardNext, 2)
-          }
-          break;
-          case '--discard-prev':
-            indexDiscardPrev = myArr.indexOf(myArr[i]);
-            if (indexDiscardPrev === 0) {
-              myArr.splice(indexDiscardPrev, 1);
-            } else {
-              myArr.splice(indexDiscardPrev - 1, 2)
-            }
-            break;
-            case '--double-next':
-              indexDoubleNext = myArr.indexOf(myArr[i]);
-              if (indexDoubleNext === length) {
-                myArr.splice(indexDoubleNext, 1);
-              } else {
-                myArr.splice(indexDoubleNext, 1, myArr[i + 1]);
-              }
-              break;
-              case '--double-prev':
-                indexDoublePrev = myArr.indexOf(myArr[i]);
-              if (indexDoublePrev === 0) {
-                myArr.splice(indexDoublePrev, 1);
-              } else {
-                myArr.splice(indexDoublePrev, 1, myArr[i - 1]);
-              }
-              break;              
-      }
-      if (myArr[i] === 'undefined') {
-        myArr.splice(myArr[i], 1);
-      }
-    }
-    
-    
-    
-
-
-
-
-    return myArr;
+  if (!Array.isArray(arr)) {
+    throw new Error();
   }
-}
-  else new Error('Error');
+  myArr = [];
+  let iNext;
+  for (let i = 0; i < arr.length; i++) {
+    let iPrev = i - 1;
+    let elemArr = arr[i];
+    if (elemArr === "--discard-next") {
+      iNext = i + 1;
+      i++;
+    } else if (elemArr === "--discard-prev") {
+      if (myArr.length && iNext !== iPrev) {
+        myArr.pop();
+      }
+    } else if (elemArr === "--double-prev") {
+      if (myArr.length && iNext !== iPrev) {
+        myArr.push(myArr[myArr.length - 1]);
+      }
+    } else if (elemArr === "--double-next") {
+      if (i < arr.length - 1) {
+        myArr.push(arr[i + 1]);
+      }
+    } else {
+      myArr.push(arr[i]);
+    }
+  }
+  return myArr;
 };
