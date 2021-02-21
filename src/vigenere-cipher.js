9c78a26b11605d113ad76369ff16f4909d32c54f
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-21 20:13:40 +0300

    feat: add class VigenereCipheeringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,71 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value) {
+    this.type = value;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error();
+    }
+
+    let latinAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let strUpper = message.toUpperCase();
+    let keyUpper = key.toUpperCase();
+    let result = "";
+    let j = 0;
+
+    for (let i = 0; i < strUpper.length; i++) {
+      if (latinAlphabet.indexOf(strUpper.split("")[i]) !== -1) {
+        let position = 0;
+        position +=
+          (latinAlphabet.indexOf(strUpper.split("")[i]) +
+            latinAlphabet.indexOf(keyUpper.split("")[j])) %
+          26;
+        result += latinAlphabet.split("")[position];
+        j >= keyUpper.length - 1 ? (j = 0) : j++;
+      } else {
+        result += message.split("")[i];
+      }
+    }
+
+    if (this.type === undefined || this.type === true) {
+      return result.toUpperCase();
+    } else {
+      return result.split("").reverse().join("").toUpperCase();
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error();
+    }
+
+    let latinAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let strUpper = encryptedMessage.toUpperCase();
+    let keyUpper = key.toUpperCase();
+    let result = "";
+    let j = 0;
+
+    for (let i = 0; i < strUpper.length; i++) {
+      if (latinAlphabet.indexOf(strUpper.split("")[i]) !== -1) {
+        let position = 0;
+        position +=
+          (latinAlphabet.indexOf(strUpper.split("")[i]) +
+            26 -
+            latinAlphabet.indexOf(keyUpper.split("")[j])) %
+          26;
+        result += latinAlphabet.split("")[position];
+        j >= keyUpper.length - 1 ? (j = 0) : j++;
+      } else {
+        result += encryptedMessage.split("")[i];
+      }
+    }
+
+    if (this.type === undefined || this.type === true) {
+      return result.toUpperCase();
+    } else {
+      return result.split("").reverse().join("").toUpperCase();
+    }
   }
 }
 
