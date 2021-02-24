4cd001a6e90b8e4ba959ed837c82cc55f1092a69
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-24 22:55:57 +0300

    feat: change function dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity === "string" && sampleActivity === "undefined") {
     const age = 0
     let ratioContent = 0
-    ratioContent = 15 / Number(sampleActivity) //соотношение содержания углерода-14 в живой ткани и его содержания в образце
+    ratioContent = 15 / parseInt(sampleActivity) //соотношение содержания углерода-14 в живой ткани и его содержания в образце
     age = (Math.log(ratioContent) / Math.log(2)) * 5730 //t = [ln (No/Nf) / ln (2)] x t½. 5730 - период полураспада углерода-14
     return Math.ceil(age) //Округляет аргумент до ближайшего большего целого.
   } else return false
