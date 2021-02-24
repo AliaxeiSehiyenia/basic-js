a294c45eb916e98f3f21437612fc471116bd5c77
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-24 22:58:03 +0300

    feat: change function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -5,7 +5,7 @@ module.exports = function repeater(str, options) {
   let addition = ""
   let additionRepeatTimes = 0
   let additionSeparator = "|"
-  for (var key in options) { // перебор массива и инициализация переменных
+  for (let key in options) { // перебор массива и инициализация переменных
     if (key == "repeatTimes") repeatTimes = options[key]
     else if (key == "separator") separator = options[key]
     else if (key == "addition") addition = options[key]
@@ -13,12 +13,12 @@ module.exports = function repeater(str, options) {
     else if (key == "additionSeparator") additionSeparator = options[key]
   }
   function repeatForAddition(str, n, separator) { // повторяет подстроку с addition
-    var new_str = ""
+    let new_str = ""
     while (n-- > 1) new_str += str + separator
     return (new_str += str)
   }
   function repeatForStr(str, n, strAfterAdd, separator) { // повторяет строку с добавление подстроки
-    var new_str = ""
+    let new_str = ""
     while (n-- > 1) new_str += str + strAfterAdd + separator
     return (new_str += str + strAfterAdd)
   }
