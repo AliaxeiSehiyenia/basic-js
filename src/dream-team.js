16a78d1e7da6c23fc9a95d86763470fca4fc9898
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-24 14:53:51 +0300

    feat: change file dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,7 +1,7 @@
 module.exports = function createDreamTeam(members) {
   if (Array.isArray(members)) {
     let arrLetters = []
-    const regexp = /\w/g // [A-Za-z0-9_]
+    const regexp = /\w/g //[A-Za-z0-9_]
     for (let i = 0; i < members.length; i++) {
       if (typeof members[i] === "string") {
         const stringWithoutSpace = members[i].match(regexp) //без пробелов.
@@ -10,7 +10,7 @@ module.exports = function createDreamTeam(members) {
     }
     for (let k = 0; k < arrLetters.length; k++) {
       arrLetters[k] = arrLetters[k].toUpperCase()
-    } 
+    } // если элементы будут с разным регистром, сортировака будет работать не коректно
 
     return arrLetters.sort().join("") //sort() - сравнивает в соответствии с порядком в Unicode.
   } else {
