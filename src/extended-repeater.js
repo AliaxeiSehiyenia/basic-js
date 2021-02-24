53d5748c5edfb17e5cc312667f5a4d6b01f734d1
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-24 12:44:20 +0300

    feat: change function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,31 +1,27 @@
 module.exports = function repeater(str, options) {
-  let stringForRepeater = String(str);
+  const stringForRepeater = String(str);
   let repeatTimes = 0;
   let separator = "+";
-  let addition = "";
-  let additionRepeatTimes = 0;
-  let additionSeparator = "|";
-  for (var key in options) {
-    if (key == "repeatTimes") repeatTimes = options[key];
-    else if (key == "separator") separator = options[key];
-    else if (key == "addition") addition = options[key];
-    else if (key == "additionRepeatTimes") additionRepeatTimes = options[key];
-    else if (key == "additionSeparator") additionSeparator = options[key];
+  let addition = ""
+  let additionRepeatTimes = 0
+  let additionSeparator = "|"
+  for (var key in options) { // перебор массива и инициализация переменных
+    if (key == "repeatTimes") repeatTimes = options[key]
+    else if (key == "separator") separator = options[key]
+    else if (key == "addition") addition = options[key]
+    else if (key == "additionRepeatTimes") additionRepeatTimes = options[key]
+    else if (key == "additionSeparator") additionSeparator = options[key]
   }
-  function repeatForAddition(str, n, separator) {
-    var new_str = "";
-    while (n-- > 1) new_str += str + separator;
-    return (new_str += str);
+  function repeatForAddition(str, n, separator) { // повторяет подстроку с addition
+    var new_str = ""
+    while (n-- > 1) new_str += str + separator
+    return (new_str += str)
   }
-  function repeatForStr(str, n, strAfterAdd, separator) {
-    var new_str = "";
-    while (n-- > 1) new_str += str + strAfterAdd + separator;
-    return (new_str += str + strAfterAdd);
+  function repeatForStr(str, n, strAfterAdd, separator) { // повторяет строку с добавление подстроки
+    var new_str = ""
+    while (n-- > 1) new_str += str + strAfterAdd + separator
+    return (new_str += str + strAfterAdd)
   }
-  let strAfterAdd = repeatForAddition(
-    addition,
-    additionRepeatTimes,
-    additionSeparator
-  );
-  return repeatForStr(stringForRepeater, repeatTimes, strAfterAdd, separator);
-};
+  const strAfterAdd = repeatForAddition(addition, additionRepeatTimes, additionSeparator)
+  return repeatForStr(stringForRepeater, repeatTimes, strAfterAdd, separator)
+}
