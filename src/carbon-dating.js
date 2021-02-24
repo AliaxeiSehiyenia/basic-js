4b1afb9095b2c6eafb0ca9d7b16501eb040f45bd
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-24 11:26:50 +0300

    feat: change function dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,9 @@
 module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity === "string" && sampleActivity === "undefined") {
-    let age = 0;
-    let ratioContent = 0;
-    ratioContent = 15 / Number(sampleActivity);
-    age = (Math.log(ratioContent) / Math.log(2)) * 5730;
-    return Math.ceil(age);
-  } else return false;
+    const age = 0
+    let ratioContent = 0
+    ratioContent = 15 / Number(sampleActivity)
+    age = (Math.log(ratioContent) / Math.log(2)) * 5730
+    return Math.ceil(age)
+  } else return false
 };
