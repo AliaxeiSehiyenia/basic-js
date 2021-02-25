65b196812ea3d9d76448c7c62290f5eed602a41b
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-25 15:52:25 +0300

    feat: change object chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,15 +4,15 @@ const chainMaker = {
     return this.str.length;
   },
   addLink(value) {
-    this.str.push(value);
-    return this;
+    this.str.push(value);// добавляет value в str
+    return this; 
   },
   removeLink(position) {
     if (position <= 0 || position > this.str.length) {
       this.str = [];
       throw new Error();
     }
-    this.str.splice(position - 1, 1);
+    this.str.splice(position - 1, 1);//удаляет с индексом position - 1
     return this;
   },
   reverseChain() {
@@ -20,9 +20,9 @@ const chainMaker = {
     return this;
   },
   finishChain() {
-    let net = `( ${this.str[0]} )`;
+    let net = `( ${this.str[0]} )`; //добавляет нулевой элемент строки с пробелами
     for (let i = 1; i < this.str.length; i++) {
-      net += `~~( ${this.str[i]} )`;
+      net += `~~( ${this.str[i]} )`; //если элементов в строке больше чем один, то добавляем ~~ и элемент с пробелами
     }
     this.str = [];
     return net;
