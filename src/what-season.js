679e72b30c86feb5e3b0f4df52c8c7d13362ffde
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-25 14:58:23 +0300

    feat: change function getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -1,10 +1,12 @@
 module.exports = function getSeason(date) {
-  let error = "Unable to determine the time of year!";
+  const error = "Unable to determine the time of year!";
   if (!date) {
     return error;
   }
   if (Date.prototype.isPrototypeOf(date)) {
-    let month = date.getUTCMonth();
+    let month = date.getUTCMonth(); 
+    /* Метод getUTCMonth() возвращает месяц указанной даты по всемирному координированному времени,
+     нумерация месяцев начинается с нуля для первого месяца в году. */
     if ((month >= 0 && month <= 1) || month == 11) {
       return "winter";
     } else if (month >= 2 && month <= 4) {
