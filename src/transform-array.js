2ea86d0b73de340910aa88fcd982bfa651ff251c
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-25 14:51:27 +0300

    feat: change function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,22 +5,20 @@ module.exports = function transform(arr) {
   myArr = [];
   let iNext;
   for (let i = 0; i < arr.length; i++) {
-    const iPrev = i - 1;
-    const elemArr = arr[i];
-    if (elemArr === "--discard-next") {
+    if (arr[i] === "--discard-next") {
       iNext = i + 1;
-      i++;
-    } else if (elemArr === "--discard-prev") {
-      if (myArr.length && iNext !== iPrev) {
-        myArr.pop();
+      i++;//не записываем в новый массив элемент равный строке "--discard-next" и пропускаем следующее заначение
+    } else if (arr[i] === "--discard-prev") {
+      if (myArr.length && iNext !== i - 1) {
+        myArr.pop(); //Метод pop() удаляет последний элемент из массива и возвращает его значение.
       }
-    } else if (elemArr === "--double-prev") {
-      if (myArr.length && iNext !== iPrev) {
-        myArr.push(myArr[myArr.length - 1]);
+    } else if (arr[i] === "--double-prev") {
+      if (myArr.length && iNext !== i - 1) { // проверяем вот такую ситуацию [1, 2, 3, "--discard-next", 4, "--double-prev", 5], 
+        myArr.push(myArr[myArr.length - 1]); // когда элемент 4 уже удален и "--double-prev" ничего не удваивает
       }
-    } else if (elemArr === "--double-next") {
+    } else if (arr[i] === "--double-next") {
       if (i < arr.length - 1) {
-        myArr.push(arr[i + 1]);
+        myArr.push(arr[i + 1]); //Метод push() добавляет один или более элементов в конец массива и возвращает новую длину массива.
       }
     } else {
       myArr.push(arr[i]);
