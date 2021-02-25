0b2aa5ffbe4203743327b3a42b85bb3d4e91c8bc
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2021-02-25 09:01:04 +0300

    feat: change new cycle for in function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,13 +1,13 @@
 module.exports = function createDreamTeam(members) {
   if (Array.isArray(members)) {
     let arrLetters = []
-    for (let i = 0; i < members.length; i++) {
-      if (typeof members[i] === "string") {
-        const stringWithoutSpace = members[i].trim() //без пробелов.
+    for (let member of members) {
+      if (typeof member === "string") {
+        const stringWithoutSpace = member.trim() //без пробелов.
         arrLetters.push(stringWithoutSpace[0])
       }
     }
-    for (let k = 0; k < arrLetters.length; k++) {
+    for (let k = 0; k < arrLetters.length; k++) {  // Возник вопрос. Почему не работает в новом синтаксисе?
       arrLetters[k] = arrLetters[k].toUpperCase()
     } // если элементы будут с разным регистром, сортировака будет работать не коректно
 
